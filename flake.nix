41c728ff9a3690287151db49170948e2e726a1ae
Mr Z <denver.cfman@gmail.com>
2026-06-28 20:29:09 -0600

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -64,10 +64,7 @@
           configurePhase = ''
             # 1. Clean up
             make distclean
-            cp ${chip-tools-src}/configs/CHIP_defconfig configs/CHIP_defconfig
-            # 2. Load the C.H.I.P. config
-            make CHIP_defconfig $makeFlags
-            make olddefconfig $makeFlags
+            make sun5i_defconfig $makeFlags
             patchShebangs scripts/
             sed -i 's/SWIG_Python_AppendOutput/SWIG_AppendOutput/g' scripts/dtc/pylibfdt/libfdt.i_shipped
             
